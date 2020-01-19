c633866671ec66cd51889611f20dabea60b6aab6
Jorge <gkitas10@gmail.com>
2020-01-19 17:02:59 -0600

    cambio de connection string

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if(process.env.NODE_ENV==='dev'){
     urlDB='mongodb://localhost:27017/cafe';
 }else{
-   urlDB='mongodb+srv://gkitas10:kx40PfCGLb2vmDPg@cluster0-wrfnm.mongodb.net/test?retryWrites=true&w=majority/cafe';
+   urlDB=process.env.MONGO_URI;
 }
 process.env.URLDB=urlDB;
 
